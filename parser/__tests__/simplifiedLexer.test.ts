bf817509371acf0171be146188a62431812c6cf5
Colin Lambrechts <developer@koolin.eu>
2023-04-28 16:15:30 +0200

    Add Simplified Lexer as experiment.

---
+++ parser/__tests__/simplifiedLexer.test.ts
@@ -0,0 +1,134 @@
+'use strict'
+
+import { expect } from '@jest/globals'
+import * as fs from 'fs'
+import { IToken, TokenType, tokenMatcher } from 'chevrotain'
+import {
+  String,
+  CurlyBraceLeft,
+  CurlyBraceRight,
+  lex,
+  Identifier,
+  Comma,
+  Integer,
+  Assignment,
+  All,
+  Equals,
+  Property,
+  URL,
+  Keyword,
+} from '../simplifiedLexer'
+
+describe('Lexer', () => {
+  it('Can lex an empty file', () => {
+    const inputText = ''
+    const lexingResult = lex(inputText)
+
+    expect(lexingResult.errors).toHaveLength(0)
+
+    const tokens = lexingResult.tokens
+
+    expect(tokens).toHaveLength(0)
+  })
+
+  it('Can lex an invalid input', () => {
+    const inputText = readDsl(Examples.Invalid)
+    const lexingResult = lex(inputText)
+
+    expect(lexingResult.errors).toHaveLength(0)
+
+    const tokens = lexingResult.tokens
+
+    expect(tokens).toHaveLength(8)
+
+    expect(tokens[0].image).toBe('This')
+    expect(tokenMatcher(tokens[0], Identifier)).toBe(true)
+    expect(tokens[1].image).toBe('is')
+    expect(tokenMatcher(tokens[1], Identifier)).toBe(true)
+    expect(tokens[2].image).toBe('not')
+    expect(tokenMatcher(tokens[2], Identifier)).toBe(true)
+    expect(tokens[3].image).toBe('a')
+    expect(tokenMatcher(tokens[3], Identifier)).toBe(true)
+    expect(tokens[4].image).toBe('valid')
+    expect(tokenMatcher(tokens[4], Identifier)).toBe(true)
+    expect(tokens[5].image).toBe('workspace')
+    expect(tokenMatcher(tokens[5], Keyword)).toBe(true)
+    expect(tokens[6].image).toBe(',')
+    expect(tokenMatcher(tokens[6], Comma)).toBe(true)
+    expect(tokens[7].image).toBe('muahahaha')
+    expect(tokenMatcher(tokens[7], Identifier)).toBe(true)
+  })
+
+  it('Can lex an empty workspace', () => {
+    const inputText = readDsl(Examples.EmptyWorkspace)
+    const lexingResult = lex(inputText)
+
+    expect(lexingResult.errors).toHaveLength(0)
+
+    const tokens = lexingResult.tokens
+
+    expect(tokens).toHaveLength(4)
+
+    expect(tokens[0].image).toBe('workspace')
+    expect(tokenMatcher(tokens[0], Keyword)).toBe(true)
+    expect(tokens[1].image).toBe('"Test Workspace"')
+    expect(tokenMatcher(tokens[1], String)).toBe(true)
+    expect(tokens[2].image).toBe('{')
+    expect(tokenMatcher(tokens[2], CurlyBraceLeft)).toBe(true)
+    expect(tokens[3].image).toBe('}')
+    expect(tokenMatcher(tokens[3], CurlyBraceRight)).toBe(true)
+  })
+
+  it('Can lex a simple workspace', () => {
+    const inputText = readDsl(Examples.workspace)
+    const lexingResult = lex(inputText)
+
+    expect(lexingResult.errors).toHaveLength(0)
+
+    const tokens = lexingResult.tokens
+
+    expect(tokens).toHaveLength(191)
+
+    VerifyAmountOfTokenType(tokens, Keyword, 43)
+    // VerifyAmountOfTokenType(tokens, Path, 1)
+    VerifyAmountOfTokenType(tokens, Property, 1)
+    VerifyAmountOfTokenType(tokens, Assignment, 9)
+    VerifyAmountOfTokenType(tokens, All, 2)
+    VerifyAmountOfTokenType(tokens, Equals, 1)
+    VerifyAmountOfTokenType(tokens, URL, 1)
+    VerifyAmountOfTokenType(tokens, Identifier, 47)
+    VerifyAmountOfTokenType(tokens, String, 35)
+    VerifyAmountOfTokenType(tokens, Integer, 6)
+    VerifyAmountOfTokenType(tokens, Comma, 0)
+    VerifyAmountOfTokenType(tokens, CurlyBraceLeft, 12)
+    VerifyAmountOfTokenType(tokens, CurlyBraceRight, 12)
+  })
+})
+
+function VerifyAmountOfTokenType(
+  tokens: IToken[],
+  tokenType: TokenType,
+  expectedAmount: number
+) {
+  expect(tokens.filter(token => tokenMatcher(token, tokenType))).toHaveLength(
+    expectedAmount
+  )
+}
+
+function readDsl(example: Examples): string {
+  const enumName = Examples[example]
+  const content = fs.readFileSync(
+    `./structurizr_examples/${enumName}.dsl`,
+    'utf8'
+  )
+  return content
+}
+
+/**
+ * Enum depicting the different examples that are present for parsing
+ */
+enum Examples {
+  Invalid,
+  EmptyWorkspace,
+  workspace,
+}
---
+++ parser/simplifiedLexer.ts
@@ -0,0 +1,82 @@
+import { Lexer, createToken, ILexingResult } from 'chevrotain'
+
+export const Identifier = createToken({
+  name: 'Identifier',
+  pattern: /[a-zA-Z]\w*/,
+})
+
+export const WhiteSpace = createToken({
+  name: 'Whitespace',
+  pattern: /\s+/,
+  group: Lexer.SKIPPED,
+})
+export const Comma = createToken({ name: 'Comma', pattern: /,/ })
+export const Assignment = createToken({ name: 'Assignment', pattern: /=/ })
+export const Equals = createToken({ name: 'Equals', pattern: /==/ })
+export const CurlyBraceLeft = createToken({
+  name: 'Curly Brace Left',
+  pattern: /{/,
+})
+export const CurlyBraceRight = createToken({
+  name: 'Curly Brace Right',
+  pattern: /}/,
+})
+export const Comment = createToken({ name: 'Comment', pattern: /\/\/.*/ })
+export const CommentMultiline = createToken({
+  name: 'Comment',
+  pattern: /\/\*.*\*\//,
+})
+export const Integer = createToken({ name: 'Integer', pattern: /0|[1-9]\d*/ })
+export const String = createToken({ name: 'String', pattern: /".*"/ })
+
+export const Property = createToken({
+  name: 'Property',
+  pattern: /[a-zA-Z]+\.[a-zA-Z]+/,
+})
+export const Keyword = createToken({
+  name: 'Keyword',
+  pattern:
+    /(workspace)|(model)|(views)|(properties)|(systemLandscape)|(systemContext)|(container)|(system)|(softwareSystem)|(person)|(!include)|(description)|(title)|(technology)|(include)|(exclude)|(autoLayout)|(tb)|(bt)|(lr)|(rl)|(theme)/,
+})
+export const Relation = createToken({ name: 'Relation', pattern: /->/ })
+export const All = createToken({ name: 'All', pattern: /\*/ })
+export const URL = createToken({
+  name: 'URL',
+  pattern:
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/,
+})
+
+const allTokens = [
+  WhiteSpace,
+  Comma,
+  CurlyBraceLeft,
+  CurlyBraceRight,
+  Equals,
+  Assignment,
+  Comment,
+  CommentMultiline,
+  Integer,
+  String,
+
+  // "keywords" appear before the Identifier
+  Property,
+  Keyword,
+  Relation,
+  All,
+  URL,
+
+  // The Identifier must appear after the keywords because all keywords are valid identifiers.
+  Identifier,
+]
+
+const SelectLexer = new Lexer(allTokens)
+
+export function lex(inputText: string): ILexingResult {
+  const lexingResult = SelectLexer.tokenize(inputText)
+
+  // if (lexingResult.errors.length > 0) {
+  //   throw Error('Sad Sad Model, lexing errors detected')
+  // }
+
+  return lexingResult
+}
